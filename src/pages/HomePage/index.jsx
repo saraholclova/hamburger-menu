6affb89ef3436e3fe2cb95fbf60016198336eb12
Sára Hölclová <sara.holclova@gmail.com>
2024-05-24 15:55:52 +0200

    zavření a otevření menu při kliku na button a na odkazy v menu

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import { MenuItem } from '../../components/MenuItem';
 import './style.css';
 
 export const HomePage = () => {
+  const [menuOpened, setMenuOpened] = useState(true);
+
+  const handleSelectItem = () => {
+    setMenuOpened(!menuOpened);
+  };
+
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={menuOpened ? 'menu' : 'menu--closed'}>
+          <button
+            onClick={() => setMenuOpened(!menuOpened)}
+            className="menu__btn"
+          ></button>
           <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
+            <MenuItem text="Domů" onSelect={handleSelectItem} />
+            <MenuItem text="Naše nabídka" onSelect={handleSelectItem} />
+            <MenuItem text="Náš tým" onSelect={handleSelectItem} />
+            <MenuItem text="Blog" onSelect={handleSelectItem} />
+            <MenuItem text="Kontakt" onSelect={handleSelectItem} />
           </div>
         </div>
       </header>
